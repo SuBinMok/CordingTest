aa4ca094ed1a28c361026b3fac988178b4e72eb1
SuBin Mok <52149840+SuBinMok@users.noreply.github.com>
2024-05-04 22:25:54 +0900

    [level 0] Title: k의 개수, Time: 21.66 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120887. k의 개수/k의 개수.py
@@ -0,0 +1,8 @@
+def solution(i, j, k):
+    answer = 0
+    for a in range(i, j+1):
+        a = str(a)
+        for b in a:
+            if b == str(k):
+                answer+=1
+    return answer
