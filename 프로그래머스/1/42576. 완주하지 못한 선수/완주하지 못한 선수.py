23695141ab9d55c88fc19ecc15a085efe535668a
SuBin Mok <52149840+SuBinMok@users.noreply.github.com>
2024-07-30 16:27:49 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 48.13 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,10 +1,9 @@
 def solution(participant, completion):
-    answer = ''
-    temp = 0
     dic = {}
+    temp = 0
     for p in participant:
-        dic[hash(p)] = p #hash[p] => 고유 값
-        temp += int(hash(p)) #같은 값이 두개 있으면 두번 더하는 것
+        dic[hash(p)] = p
+        temp += hash(p)
     for c in completion:
-        temp -= hash(c) # completion에 중복된 값이 없으면 temp에는 p에서 더한 중복 값이 남아 있음
+        temp -= hash(c)
     return dic[temp]
