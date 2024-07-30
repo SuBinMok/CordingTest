a745a2d60d19b4f6dcf62dac6ecd8681f86b0f2b
SuBin Mok <52149840+SuBinMok@users.noreply.github.com>
2024-07-30 16:02:04 +0900

    [level 1] Title: 폰켓몬, Time: 315.91 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,5 +1,11 @@
 def solution(nums):
-    nl = list(set(nums))
-    if (len(nums)/2) < len(nl):
-        return int(len(nums)/2)
-    return len(nl)
+    answer = 0
+    choose = int(len(nums)/2)
+    arr = []
+    for i in nums:
+        if i not in arr:
+            arr.append(i)
+    if len(arr) >= choose:
+        return choose
+    else:
+        return len(arr)
