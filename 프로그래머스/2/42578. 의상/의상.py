e6d939ba1524f21e258720a8ee0f8bcba99964d8
SuBin Mok <52149840+SuBinMok@users.noreply.github.com>
2024-07-31 18:53:44 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,11 +1,11 @@
 def solution(clothes):
-    wear = {}
     answer = 1
-    for c, w in clothes:
-        if w in wear:
-            wear[w].append(c) #append로 새 값을 추가할 수 있게 함
+    wear_dic = {}
+    for cloth, wear in clothes:
+        if wear in wear_dic:
+            wear_dic[wear].append(cloth)
         else:
-            wear[w] = [c] #리스트 형태로 딕셔너리에 추가해서
-    for k in wear.keys():
-        answer *= (len(wear[k])+1) # 답은 (dic[key]+1) 크기만큼 곱해나감.
-    return answer - 1 
+            wear_dic[wear] = [cloth]
+    for i in wear_dic.keys():
+        answer *= (len(wear_dic[i])+1)
+    return answer -1
