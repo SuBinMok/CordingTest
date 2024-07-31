045e6abf45d021a14c3fcca6223b09b2de92ea09
SuBin Mok <52149840+SuBinMok@users.noreply.github.com>
2024-07-31 19:50:01 +0900

    [level 3] Title: 베스트앨범, Time: 0.17 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,20 +1,21 @@
 def solution(genres, plays):
     answer = []
-    p = {}
+    playlist = {}
     arr = []
-    for i in range(len(genres)):
+    num = len(genres)
+    for i in range(num):
         arr.append([i, genres[i], plays[i]])
     arr = sorted(arr, reverse = True, key = lambda x: x[2])
-    for i in range(len(genres)):
-        if arr[i][1] in p:
-            p[arr[i][1]] += arr[i][2]
+    for i in range(num):
+        if arr[i][1] in playlist:
+            playlist[arr[i][1]] += arr[i][2]
         else:
-            p[arr[i][1]] = arr[i][2]
-    for i, v in sorted(p.items(), reverse = True, key = lambda x: x[1]):
-        k = 0
-        for j in range(len(genres)):
-            if arr[j][1] == i and k < 2 and v >= 0:
-                answer.append(arr[j][0])
-                v -= arr[j][2]
+            playlist[arr[i][1]] = arr[i][2]
+    for item, values in sorted(playlist.items(), reverse = True, key = lambda x: x[1]):
+        k=0
+        for i in range(num):
+            if arr[i][1] == item and k<2 and values >= 0:
+                answer.append(arr[i][0])
+                values -= arr[i][0]
                 k += 1
     return answer
