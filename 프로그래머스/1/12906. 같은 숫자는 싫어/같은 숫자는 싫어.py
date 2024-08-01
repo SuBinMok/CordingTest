f526b220feedd1a022b61b0801b28ac568aebd71
SuBin Mok <52149840+SuBinMok@users.noreply.github.com>
2024-08-01 16:38:43 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 110.67 ms, Memory: 27.9 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,13 +1,10 @@
 def solution(arr):
     answer = []
-    for i in range(len(arr)-1):
-        if arr[i] == arr[i+1] and i == len(arr)-2:
+    arr.append(-1)
+    for i in range(len(arr) - 1):
+        if arr[i] != arr[i+1]:
             answer.append(arr[i])
-        elif arr[i] != arr[i+1] and i == len(arr)-2:
-            answer.append(arr[i])
-            answer.append(arr[i+1])
-            break
-        elif arr[i] != arr[i+1]:
-            answer.append(arr[i])
-        
+        else:
+            continue
+    
     return answer
