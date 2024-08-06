5fd53de4238809273544b638b6832b6913c22789
SuBin Mok <52149840+SuBinMok@users.noreply.github.com>
2024-08-06 17:28:19 +0900

    [level 2] Title: 더 맵게, Time: 1629.33 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,38 +1,15 @@
-#heapq이용 : 케이스 모두 통과 및 효율성 모두 통과
 import heapq
 def solution(scoville, K):
-    heapq.heapify(scoville) # 리스트 scoville을 heapq로 대체하는 것
-    answer = 0
+    answer = 0    
+    heapq.heapify(scoville)
     if scoville[0] >= K:
         return 0
     while scoville[0] < K:
         if len(scoville) == 1:
             return -1
-        first = heapq.heappop(scoville)
-        second = heapq.heappop(scoville)
-        heapq.heappush(scoville, first+second*2)
+        a = heapq.heappop(scoville)
+        b = heapq.heappop(scoville)
+        heapq.heappush(scoville, a + (b*2))
         answer+=1
+        
     return answer
-
-
-
-# 우선순위 큐 이용 : 케이스 모두 통과 및 효율성 2, 3, 5번 통과.
-# 우선순위 큐가 heapq보다 더 느린듯.. 
-# from queue import PriorityQueue
-# def solution(scoville, K):
-#     q = PriorityQueue(len(scoville))
-#     for i in scoville:
-#         q.put(i)
-#     answer = 0
-#     while not q.empty():
-#         first = q.get()
-#         size = q.qsize()
-#         if size == 0 and first < K:
-#             return -1
-#         elif size != 0 and first < K:
-#             second = q.get()
-#             q.put(first + (second*2))
-#             answer += 1
-#         elif first >= K:
-#             break
-#     return answer
