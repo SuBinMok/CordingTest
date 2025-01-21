fdc15748cfac303651ec817ecf5aa2ba81a64592
SuBin Mok <52149840+SuBinMok@users.noreply.github.com>
2025-01-21 11:03:38 +0900

    [Silver III] Title: 1로 만들기, Time: 480 ms, Memory: 40224 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,13 @@
+import sys
+
+N = int(sys.stdin.readline())
+D = [999]*(N+1)
+D[1] = 0
+
+for i in range(2, N+1):
+    D[i] = D[i-1]+1
+    if i%2 == 0:
+        D[i] = min(D[i], D[i//2]+1)
+    if i%3 == 0:
+        D[i] = min(D[i], D[i//3]+1)
+print(D[N])
