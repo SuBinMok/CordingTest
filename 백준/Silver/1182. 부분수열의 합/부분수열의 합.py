8f004d14f1d7713ca6cc9a524c80fc11db9b3183
SuBin Mok <52149840+SuBinMok@users.noreply.github.com>
2025-01-29 10:33:10 +0900

    [Silver II] Title: 부분수열의 합, Time: 212 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -0,0 +1,16 @@
+import sys
+n, s = map(int, sys.stdin.readline().split())
+arr = list(map(int, sys.stdin.readline().split()))
+cnt = 0
+def func(cur, tot):
+    global cnt
+    if cur == n:
+        if tot == s:
+            cnt+= 1
+        return
+    func(cur+1, tot)
+    func(cur+1, tot+arr[cur])
+func(0, 0)
+if s == 0:
+    cnt-=1
+print(cnt)
