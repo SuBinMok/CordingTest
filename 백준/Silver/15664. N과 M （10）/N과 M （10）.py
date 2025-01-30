17be040a4e8d49e4d65eab0efac9df7086c18229
SuBin Mok <52149840+SuBinMok@users.noreply.github.com>
2025-01-30 12:10:24 +0900

    [Silver II] Title: N과 M (10), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,24 @@
+import sys
+n, s = map(int, sys.stdin.readline().split())
+arr = list(map(int, sys.stdin.readline().split()))
+arr = sorted(arr)
+
+visited = [0]*n
+result = []
+def func():
+    check = 0
+    if len(result) == s:
+        print(*result)
+        return
+    for i in range(n):
+        if result and arr[i] < result[-1]:
+            continue
+        if check != arr[i] and not visited[i]:
+
+            visited[i] = True
+            result.append(arr[i])
+            check = arr[i]
+            func()
+            result.pop()
+            visited[i] = False
+func()
