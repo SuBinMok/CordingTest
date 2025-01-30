50faf698b6eac53c7a8552f0ce35b55adea6d423
SuBin Mok <52149840+SuBinMok@users.noreply.github.com>
2025-01-30 10:55:54 +0900

    [Silver III] Title: N과 M (3), Time: 1916 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,19 @@
+import sys
+n, s = map(int, sys.stdin.readline().split())
+visited = [0]*n
+result = []
+
+def func():
+    if len(result) == s:
+        print(*result)
+        return
+    for i in range(n):
+        # if result and i+1 <= result[-1]:
+        #     continue
+        visited[i] = True
+        result.append(i+1)
+        func()
+        visited[i] = False
+        result.pop()
+
+func()
