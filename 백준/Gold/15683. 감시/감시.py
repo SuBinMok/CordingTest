7666cac47577ecd7af6a9228068b014959815af9
SuBin Mok <52149840+SuBinMok@users.noreply.github.com>
2025-04-15 11:31:21 +0900

    [Gold III] Title: 감시, Time: 536 ms, Memory: 35068 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.py
+++ 백준/Gold/15683. 감시/감시.py
@@ -1,93 +1,64 @@
-from collections import deque
 import sys
-input = sys.stdin.readline
+from collections import deque
+n, m = map(int, sys.stdin.readline().split())
+office = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 
-# 사무실의 세로(N), 가로(M)
-N, M = map(int, input().split())
-space = [list(map(int, input().split())) for _ in range(N)]
+# east, south, west, north
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
 
-# 남, 동, 북, 서 방향을 가리킴 (남쪽을 시작으로 반시계방향으로 돌아가는 순서로 되어 있음)
-dy = [1, 0, -1, 0]       # y축
-dx = [0, 1, 0, -1]       # x축
-
-# 감시해야하는 모든 방향 (각각의 cctv별로 감시할 수 있는 방향)
 direction = {
-    1: [[0], [1], [2], [3]],            # 1번cctv 방향:0, 1, 2, 3, --> 4가지
-    2: [[0, 2], [1, 3]],                    # 2번cctv 방향:(0, 2), (1, 3) --> 2가지
-    3: [[0, 1], [1, 2], [2, 3], [3, 0]],    # 3번cctv 방향:(0, 1), (1, 2), (2, 3), (3, 0) --> 4가지
-    4: [[0, 1, 2], [1, 2, 3], [2, 3, 0], [3, 0, 1]],    # 4번cctv 방향... 4가지
-    5: [[0, 1, 2, 3]]                      # 5번cctv 방향... 1가지
+    1: [[0], [1], [2], [3]],
+    2: [[0, 2], [1, 3]],
+    3: [[0, 1], [1, 2], [2, 3], [3, 0]],
+    4: [[0, 1, 2], [1, 2, 3], [2, 3, 0], [3, 0, 1]],
+    5: [[0, 1, 2, 3]]
 }
-
-# 사무실의 범위를 벗어나는지 체크해주는 함수
-def check(row, col):
-    return row < 0 or row >=N or col < 0 or col >=M
-    
-
+def check_in(x, y):
+    return x < 0 or x >= n or y < 0 or y >= m
 def init():
-    obj = deque() #  cctv의 위치를 저장할 큐 
+    cctv = []
     answer = 0
-    for i in range(N):
-        for j in range(M):
-            # 벽이아니고 빈칸이아니면 
-            if space[i][j] != 6 and space[i][j] != 0:
-                obj.append((space[i][j], i, j))   # cctv번호, cctv 좌표 저장
-            # cctv가 아에 없는 경우도 고려해서 빈칸의 갯수로 맞춰둠
-            if space[i][j] == 0:answer += 1
-    return obj, answer
-
-# cctv좌표들, 빈칸 개수 초기화
+    for i in range(n):
+        for j in range(m):
+            if office[i][j] != 6 and office[i][j] != 0:
+                cctv.append([i, j, office[i][j]])
+            if office[i][j] == 0:
+                answer += 1
+    return cctv, answer
 cctv, answer = init()
-
-
-def move(y, x, direc, space_copy):
-    # 각각의 방향에 대해서 전부 이동시켜줌
-    for d in direc:
-        ny, nx = y, x
-        
-        while True:
+def rotation(x, y, d_list, co):
+    for d in d_list: # 회전하면서
+        nx = x
+        ny = y
+        while True: # 같은 줄에 있는 0 조건에 안걸린다면 X로 바꿈
             nx += dx[d]
             ny += dy[d]
-            # 범위를 벗어났거나 벽을 만났다면 
-            if check(ny, nx) or space_copy[ny][nx] == 6:
+            if check_in(nx, ny) or co[nx][ny] == 6:
                 break
-            # 빈칸이아니면 
-            if space_copy[ny][nx] != 0:
+            if co[nx][ny] != 0:
                 continue
-            space_copy[ny][nx] = '#'
+            co[nx][ny] = "X"
 
-
-# 사각지대를 구하는 함수        
-def zero_cnt(space_copy):
+def zero_cnt(co):
     global answer
     cnt = 0
-    for i in space_copy:
+    for i in co:
         cnt += i.count(0)
-    answer = min(answer, cnt) 
-    
+    answer = min(answer, cnt)
+def dfs(lv, office):
+    copy_office = [[j for j in office[i]] for i in range(n)]
+    if lv == len(cctv): # lv : 확인하고 잇는 cctv, 마지막 cctv까지 다 봣으면
+        zero_cnt(copy_office)
+        return # 이전 상태로 ㄱㄱ
 
-# 백준 15651 N과 M(3) 문제와 유사 (백트래킹)
-def dfs(level, space):
-    # space_copy = copy.deepcopy(space)           
-    space_copy = [[j for j in space[i]] for i in range(N)]
-    # 2번째 상태가 실행되기전 바로 전 상태를 저장함 
-    # (예를 들어 2번째 상태를 시작하기 전에 1번째 상태의 결과를 저장함)
-   
-    if level == len(cctv):
-        zero_cnt(space_copy)
-        return			# 전 상태로 돌아감
-    
-    number, y, x  = cctv[level]
-    
-    # number번째 cctv에 대해 가능한 모든 방향을 순차적으로 고려 
-    for d in direction[number]:    
-        move(y, x, d, space_copy)
-        dfs(level+1, space_copy)   # level+1번째 cctv를 고려
-        space_copy = [[j for j in space[i]] for i in range(N)]
-        # space_copy = copy.deepcopy(space)
-        
-        # 하나의 상태를 return 한다음 바로 전 상태로 바꿈 
-        # 만약 2번째 상태가 끝났다면,  1번째를 수행했을 때의 결과로 바꿈
-    
-dfs(0, space)
+    x, y, num = cctv[lv]
+    for d in direction[num]:
+        rotation(x, y, d, copy_office)
+        dfs(lv+1, copy_office)
+        copy_office = [[j for j in office[i]]for i in range(n)]
+
+
+dfs(0, office)
+
 print(answer)
