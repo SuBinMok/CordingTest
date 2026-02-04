be8c3119d27fa709c4355e59ecb2198c92a623b7
SuBin Mok <52149840+SuBinMok@users.noreply.github.com>
2026-02-04 11:44:56 +0900

    [Silver I] Title: 정수 삼각형, Time: 116 ms, Memory: 36504 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,17 @@
+import sys
+n = int(sys.stdin.readline())
+arr = []
+step = []
+for _ in range(n):
+    a = list(map(int, sys.stdin.readline().split()))
+    arr.append(a)
+
+for i in range(1, n):
+    for j in range(len(arr[i])):
+        if j == 0:
+            arr[i][j] += arr[i-1][j]
+        elif j == len(arr[i])-1:
+            arr[i][j] += arr[i-1][j-1]
+        else:
+            arr[i][j] += max(arr[i-1][j-1], arr[i-1][j])
+print(max(arr[n-1]))
