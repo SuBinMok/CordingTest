4f0c0e92913a03e19338c6a91f83abee4fa3d0c9
SuBin Mok <52149840+SuBinMok@users.noreply.github.com>
2026-02-05 13:50:18 +0900

    [Silver II] Title: 연속합, Time: 64 ms, Memory: 39096 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,11 @@
+import sys
+
+n = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split()))
+dp = [0] * (n)
+dp[0] = arr[0]
+
+for i in range(1, n):
+    dp[i] = max(arr[i], arr[i] +dp[i-1])
+
+print(max(dp))
